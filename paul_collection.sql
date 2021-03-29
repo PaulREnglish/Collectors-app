38aaf09f03401e7d921461346f4f3bf7cf520e6d
Zedonk1995 <paulRenglish@yahoo.co.uk>
2021-03-29 14:24:46 +0100

    updated database to include images

--- paul_collection.sql
+++ paul_collection.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.33)
 # Database: paul_collection
-# Generation Time: 2021-03-29 10:55:16 +0000
+# Generation Time: 2021-03-29 13:24:03 +0000
 # ************************************************************
 
 
@@ -34,25 +34,26 @@ CREATE TABLE `weapons` (
   `ammo_comsumption` mediumint(11) DEFAULT NULL,
   `ammo_efficiency` enum('1','2','3','4','5','6','7','8','9','10','infinite') DEFAULT NULL,
   `default_weapon_number` tinyint(11) DEFAULT NULL,
+  `image_url` varchar(3000) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `weapons` WRITE;
 /*!40000 ALTER TABLE `weapons` DISABLE KEYS */;
 
-INSERT INTO `weapons` (`id`, `name`, `damage_per_second`, `rate_of_fire`, `ammo_comsumption`, `ammo_efficiency`, `default_weapon_number`)
+INSERT INTO `weapons` (`id`, `name`, `damage_per_second`, `rate_of_fire`, `ammo_comsumption`, `ammo_efficiency`, `default_weapon_number`, `image_url`)
 VALUES
-	(1,'Wraithverge','10','6',36,'8',4),
-	(2,'Mace of Contrition','1','5',0,'infinite',1),
-	(3,'Arc of Death','5','3',5,'3',3),
-	(4,'Frost Shards','3','4',3,'2',2),
-	(5,'Hammer of Retribution','7','3',3,'10',3),
-	(6,'Hellstaff','6','9',1,'9',5),
-	(7,'Chainsaw','3','10',0,'infinite',1),
-	(8,'Super Shotgun','5','2',2,'9',3),
-	(9,'Rocket Launcher (Doom)','8','7',1,'5',5),
-	(10,'Serpent Staff','6','8',1,'6',2),
-	(11,'Firemace','7','9',1,'1',7);
+	(1,'Wraithverge','10','6',36,'8',4,'https://www.hexenmagetower.com/charact_files/wraithverge5.JPG'),
+	(2,'Mace of Contrition','1','5',0,'infinite',1,'https://static.doomworld.com/monthly_2020_02/Screenshot_Hexen_20200221_080012.png.340d9d002265a0b15a01b1569f39237b.png'),
+	(3,'Arc of Death','5','3',5,'3',3,'https://doomwiki.org/w/images/8/8c/Hexen5.png'),
+	(4,'Frost Shards','3','4',3,'2',2,'https://doomwiki.org/w/images/4/48/Hexen2.png'),
+	(5,'Hammer of Retribution','7','3',3,'10',3,'https://doomwiki.org/w/images/c/c3/DarkBishop.png'),
+	(6,'Hellstaff','6','9',1,'9',5,'https://doomwiki.org/w/images/2/21/Hellstaff.png'),
+	(7,'Chainsaw (Doom)','3','10',0,'infinite',1,'https://doomwiki.org/w/images/thumb/3/3a/Chainsaw.jpg/250px-Chainsaw.jpg'),
+	(8,'Super Shotgun','5','2',2,'9',3,'https://doomwiki.org/w/images/thumb/7/7d/DoubleBarreled_firing2.jpg/256px-DoubleBarreled_firing2.jpg'),
+	(9,'Rocket Launcher (Doom)','8','7',1,'5',5,'https://assets1.ignimgs.com/2019/03/29/4-rocket-launcher-1553903531418.jpg'),
+	(10,'Serpent Staff','6','8',1,'6',2,'https://doomwiki.org/w/images/1/1b/Serpent_Staff_Small.PNG'),
+	(11,'Firemace','7','9',1,'1',7,'images/firemaceFiring.png');
 
 /*!40000 ALTER TABLE `weapons` ENABLE KEYS */;
 UNLOCK TABLES;
